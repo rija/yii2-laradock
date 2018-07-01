22046654756b95aa128b7c315e04baa3aa4389c3
Rija Menage <rija+git@cinecinetique.com>
2018-07-01 18:19:13 +0100

    Changes to make deployment more flexible and resilient
    * Use of COMPOSE_PROJECT_NAME in env-gigadb instead of container_name in docker-compose:
    
    I need to deploy multiple instances of the same stack of containers on the same machine.
    The use of container_name precludes multiple instances as they would be a name clash.
    Similarly the use of container_name prevents using docker-compose's scaling feature.
    Finally, most of container_name values were identical to the service name.
    Normally docker-compose creates name dynamically using the enclosing directory name for prefix
    But I needed more control so the COMPOSE_PROJECT_NAME is a special env variable recognised
    by docker-compose that sets the prefix of instanciated containers to its value.
    
    * the sql files to bootstrap the gigadb db container are sourced from the gigadb website git checkout:
    
    Because the database schema can change like normal code, there's a risk for the sql files in yii2-laradock to go stale.
    It's also create a duplication. So there is now a SQL_DIRECTORY variable in env-gigadb
    set by default to a gigadb-website checkout's sql directory
    
    * Not hardcoding the path to gigadb-website in generate_config.sh
    
    Instead, use the value of APPLICATION already in env-gigadb. This way, I can have two checkouts (different branches or different commmit version)
    and instantiate the docker stack for each. of the checkout. so all instances of /vagrant have been replace by $APPLICATION in generate_config.sh and docker-compose.yml
    
    * In generate_config.sh, the ftp examples files are not downloaded if they already present locally
    
    * In generate_config.sh, instead of git cloning Yii 1, I download the release tarball
    
    That way the script can be re-run many times without crashing at an attempt to clone in an already cloned directory.
    Also the download won't if the release tarball is already present locally.
    
    * Made sites-available read-only in the volumes config for nginx service in docker-compose.yml
    
    If I need to change the nginx config in-container (for example for debugging/profiling),
    any change will be made to the nginx site config files in yii2-laradock which is not desirable
    as it will affect any future instances of the docker stack.
    Instead,  to cater for in-container changes, I've added another read/writable volume linked to the DATA_SAVE_PATH.
    
    * the vsftpd service was referencing hard-coded path for Yii dependencies in its volumes config
    
    I've replaced them with YII_PATH
    
    * In generate_config.sh, changed each line of sed based replacement to not execute sed at all
    if the cp command failed.

--- generate_config.sh
+++ generate_config.sh
@@ -1,96 +1,118 @@
 #!/bin/bash
 
+# read env variables in same directory. 
+# They are shared by both this script and Docker compose files, so .env has to stay in yii2-laradock with this script.
+source "./.env"
+echo "Yii path: ${YII_PATH}"
+echo "Application path: ${APPLICATION}"
+echo "COMPOSER_PROJECT_NAME: ${COMPOSER_PROJECT_NAME}"
+
+
+# do the stuff that vagrant would normally do. Even if vagrant is used, doing this stuff regardless is still ok.
+mkdir -p ${APPLICATION}/protected/runtime
+mkdir -p ${APPLICATION}/assets
+mkdir -p ${APPLICATION}/images/tempcaptcha
+chmod 777 ${APPLICATION}/protected/runtime
+chmod 777 ${APPLICATION}/assets
+chmod 777 ${APPLICATION}/images/tempcaptcha
+
 # Generate config files for gigadb-website application using sed
-source /vagrant/yii2-laradock/.env
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/yii-aws.json.erb /vagrant/protected/config/aws.json
-sed -i "/<% aws = node\[:aws\] -%>/d" /vagrant/protected/config/aws.json \
-    && sed -i "s|<%= aws\[:aws_access_key_id\] %>|${AWS_ACCESS_KEY_ID}|g" /vagrant/protected/config/aws.json \
-    && sed -i "s|<%= aws\[:aws_secret_access_key\] %>|${AWS_SECRET_ACCESS_KEY}|g" /vagrant/protected/config/aws.json \
-    && sed -i "s|<%= aws\[:s3_bucket_for_file_bundles\] %>|${AWS_S3_BUCKET_FOR_FILE_BUNDLES}|g" /vagrant/protected/config/aws.json \
-    && sed -i "s|<%= aws\[:s3_bucket_for_file_previews\] %>|${AWS_S3_BUCKET_FOR_FILE_PREVIEWS}|g" /vagrant/protected/config/aws.json \
-    && sed -i "s|<%= aws\[:aws_default_region\] %>|${AWS_DEFAULT_REGION}|g" /vagrant/protected/config/aws.json
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/yii-aws.json.erb ${APPLICATION}/protected/config/aws.json \
+	&& sed -i "/<% aws = node\[:aws\] -%>/d" ${APPLICATION}/protected/config/aws.json \
+    && sed -i "s|<%= aws\[:aws_access_key_id\] %>|${AWS_ACCESS_KEY_ID}|g" ${APPLICATION}/protected/config/aws.json \
+    && sed -i "s|<%= aws\[:aws_secret_access_key\] %>|${AWS_SECRET_ACCESS_KEY}|g" ${APPLICATION}/protected/config/aws.json \
+    && sed -i "s|<%= aws\[:s3_bucket_for_file_bundles\] %>|${AWS_S3_BUCKET_FOR_FILE_BUNDLES}|g" ${APPLICATION}/protected/config/aws.json \
+    && sed -i "s|<%= aws\[:s3_bucket_for_file_previews\] %>|${AWS_S3_BUCKET_FOR_FILE_PREVIEWS}|g" ${APPLICATION}/protected/config/aws.json \
+    && sed -i "s|<%= aws\[:aws_default_region\] %>|${AWS_DEFAULT_REGION}|g" ${APPLICATION}/protected/config/aws.json
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/yii-console.php.erb /vagrant/protected/config/console.php
-sed -i "s|<%= node\[:gigadb\]\[:mfr\]\[:preview_server\] %>|${PREVIEW_SERVER_HOST}|g" /vagrant/protected/config/console.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:ftp\]\[:connection_url\] %>|${FTP_CONNECTION_URL}|g" /vagrant/protected/config/console.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:multidownload\]\[:download_host\] %>|${MULTIDOWNLOAD_SERVER_HOST}|g" /vagrant/protected/config/console.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:redis\]\[:server\] %>|${REDIS_SERVER_HOST}|g" /vagrant/protected/config/console.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:beanstalk\]\[:host\] %>|${BEANSTALK_SERVER_HOST}|g" /vagrant/protected/config/console.php
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/yii-console.php.erb ${APPLICATION}/protected/config/console.php \
+	&& sed -i "s|<%= node\[:gigadb\]\[:mfr\]\[:preview_server\] %>|${PREVIEW_SERVER_HOST}|g" ${APPLICATION}/protected/config/console.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:ftp\]\[:connection_url\] %>|${FTP_CONNECTION_URL}|g" ${APPLICATION}/protected/config/console.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:multidownload\]\[:download_host\] %>|${MULTIDOWNLOAD_SERVER_HOST}|g" ${APPLICATION}/protected/config/console.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:redis\]\[:server\] %>|${REDIS_SERVER_HOST}|g" ${APPLICATION}/protected/config/console.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:beanstalk\]\[:host\] %>|${BEANSTALK_SERVER_HOST}|g" ${APPLICATION}/protected/config/console.php
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/yii-index.php.erb /vagrant/index.php
-sed -i "/<% path = node\[:yii\]\[:path\] -%>/d" /vagrant/index.php \
-    && sed -i "s|<%= path %>|${YII_PATH}|g" /vagrant/index.php
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/yii-index.php.erb ${APPLICATION}/index.php \
+	&& sed -i "/<% path = node\[:yii\]\[:path\] -%>/d" ${APPLICATION}/index.php \
+    && sed -i "s|<%= path %>|${YII_PATH}|g" ${APPLICATION}/index.php
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/yiic.php.erb /vagrant/protected/yiic.php
-sed -i "/<% path = node\[:yii\]\[:path\] -%>/d" /vagrant/protected/yiic.php \
-    && sed -i "s|<%= path %>|${YII_PATH}|g" /vagrant/protected/yiic.php
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/yiic.php.erb ${APPLICATION}/protected/yiic.php \
+	&& sed -i "/<% path = node\[:yii\]\[:path\] -%>/d" ${APPLICATION}/protected/yiic.php \
+    && sed -i "s|<%= path %>|${YII_PATH}|g" ${APPLICATION}/protected/yiic.php
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/yii-local.php.erb /vagrant/protected/config/local.php
-sed -i "/<% home_url = node\[:gigadb\]\[:server_names\] -%>/d" /vagrant/protected/config/local.php \
-    && sed -i "/<% server_email = node\[:gigadb\]\[:admin_email\] -%>/d" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:mailchimp\]\[:mailchimp_api_key\] %>|${MAILCHIMP_API_KEY}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:mailchimp\]\[:mailchimp_list_id\] %>|${MAILCHIMP_LIST_ID}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:analytics\]\[:analytics_client_email\] %>|${ANALYTICS_CLIENT_EMAIL}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:analytics\]\[:analytics_client_id\] %>|${ANALYTICS_CLIENT_ID}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:analytics\]\[:analytics_keyfile_path\] %>|${ANALYTICS_KEYFILE_PATH}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= home_url %>|${HOME_URL}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= home_url %>|${SERVER_EMAIL}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:recaptcha\]\[:recaptcha_publickey\] %>|${RECAPTCHA_PUBLICKEY}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:recaptcha\]\[:recaptcha_privatekey\] %>|${RECAPTCHA_PRIVATEKEY}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:analytics\]\[:google_analytics_profile\] %>|${GOOGLE_ANALYTICS_PROFILE}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:mds\]\[:mds_username\] %>|${MDS_USERNAME}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:mds\]\[:mds_password\] %>|${MDS_PASSWORD}|g" /vagrant/protected/config/local.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:mds\]\[:mds_prefix\] %>|${MDS_PREFIX}|g" /vagrant/protected/config/local.php
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/yii-local.php.erb ${APPLICATION}/protected/config/local.php \
+	&& sed -i "/<% home_url = node\[:gigadb\]\[:server_names\] -%>/d" ${APPLICATION}/protected/config/local.php \
+    && sed -i "/<% server_email = node\[:gigadb\]\[:admin_email\] -%>/d" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:mailchimp\]\[:mailchimp_api_key\] %>|${MAILCHIMP_API_KEY}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:mailchimp\]\[:mailchimp_list_id\] %>|${MAILCHIMP_LIST_ID}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:analytics\]\[:analytics_client_email\] %>|${ANALYTICS_CLIENT_EMAIL}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:analytics\]\[:analytics_client_id\] %>|${ANALYTICS_CLIENT_ID}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:analytics\]\[:analytics_keyfile_path\] %>|${ANALYTICS_KEYFILE_PATH}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= home_url %>|${HOME_URL}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= home_url %>|${SERVER_EMAIL}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:recaptcha\]\[:recaptcha_publickey\] %>|${RECAPTCHA_PUBLICKEY}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:recaptcha\]\[:recaptcha_privatekey\] %>|${RECAPTCHA_PRIVATEKEY}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:analytics\]\[:google_analytics_profile\] %>|${GOOGLE_ANALYTICS_PROFILE}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:mds\]\[:mds_username\] %>|${MDS_USERNAME}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:mds\]\[:mds_password\] %>|${MDS_PASSWORD}|g" ${APPLICATION}/protected/config/local.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:mds\]\[:mds_prefix\] %>|${MDS_PREFIX}|g" ${APPLICATION}/protected/config/local.php
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/yii-main.php.erb /vagrant/protected/config/main.php
-sed -i "s|<%= node\[:gigadb\]\[:facebook\]\[:app_id\] %>|${FACEBOOK_APP_ID}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:facebook\]\[:app_secret\] %>|${FACEBOOK_APP_SECRET}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:linkedin\]\[:api_key\] %>|${LINKEDIN_API_KEY}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:linkedin\]\[:secret_key\] %>|${LINKEDIN_SECRET_KEY}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:google\]\[:client_id\] %>|${GOOGLE_CLIENT_ID}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:google\]\[:client_secret\] %>|${GOOGLE_SECRET}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:twitter\]\[:key\] %>|${TWITTER_KEY}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:twitter\]\[:secret\] %>|${TWITTER_SECRET}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:orcid\]\[:client_id\] %>|${ORCID_CLIENT_ID}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:orcid\]\[:client_secret\] %>|${ORCID_CLIENT_SECRET}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:ftp\]\[:connection_url\] %>|${FTP_CONNECTION_URL}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:redis\]\[:server\] %>|${REDIS_SERVER_HOST}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:beanstalk\]\[:host\] %>|${BEANSTALK_SERVER_HOST}|g" /vagrant/protected/config/main.php \
-    && sed -i "s|<%= node\[:gigadb\]\[:mfr\]\[:preview_server\] %>|${PREVIEW_SERVER_HOST}|g" /vagrant/protected/config/main.php
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/yii-main.php.erb ${APPLICATION}/protected/config/main.php \
+	&& sed -i "s|<%= node\[:gigadb\]\[:facebook\]\[:app_id\] %>|${FACEBOOK_APP_ID}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:facebook\]\[:app_secret\] %>|${FACEBOOK_APP_SECRET}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:linkedin\]\[:api_key\] %>|${LINKEDIN_API_KEY}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:linkedin\]\[:secret_key\] %>|${LINKEDIN_SECRET_KEY}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:google\]\[:client_id\] %>|${GOOGLE_CLIENT_ID}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:google\]\[:client_secret\] %>|${GOOGLE_SECRET}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:twitter\]\[:key\] %>|${TWITTER_KEY}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:twitter\]\[:secret\] %>|${TWITTER_SECRET}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:orcid\]\[:client_id\] %>|${ORCID_CLIENT_ID}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:orcid\]\[:client_secret\] %>|${ORCID_CLIENT_SECRET}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:ftp\]\[:connection_url\] %>|${FTP_CONNECTION_URL}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:redis\]\[:server\] %>|${REDIS_SERVER_HOST}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:beanstalk\]\[:host\] %>|${BEANSTALK_SERVER_HOST}|g" ${APPLICATION}/protected/config/main.php \
+    && sed -i "s|<%= node\[:gigadb\]\[:mfr\]\[:preview_server\] %>|${PREVIEW_SERVER_HOST}|g" ${APPLICATION}/protected/config/main.php
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/yii-db.json.erb /vagrant/protected/config/db.json
-sed -i "/<% db = node\[:gigadb\]\[:db\] -%>/d" /vagrant/protected/config/db.json \
-    && sed -i "s|<%= db\[:database\] %>|gigadb|g" /vagrant/protected/config/db.json \
-    && sed -i "s|<%= db\[:host\] %>|${GIGADB_HOST}|g" /vagrant/protected/config/db.json \
-    && sed -i "s|<%= db\[:user\] %>|${GIGADB_USER}|g" /vagrant/protected/config/db.json \
-    && sed -i "s|<%= db\[:password\] %>|${GIGADB_PASSWORD}|g" /vagrant/protected/config/db.json
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/yii-db.json.erb ${APPLICATION}/protected/config/db.json \
+	&& sed -i "/<% db = node\[:gigadb\]\[:db\] -%>/d" ${APPLICATION}/protected/config/db.json \
+    && sed -i "s|<%= db\[:database\] %>|gigadb|g" ${APPLICATION}/protected/config/db.json \
+    && sed -i "s|<%= db\[:host\] %>|${GIGADB_HOST}|g" ${APPLICATION}/protected/config/db.json \
+    && sed -i "s|<%= db\[:user\] %>|${GIGADB_USER}|g" ${APPLICATION}/protected/config/db.json \
+    && sed -i "s|<%= db\[:password\] %>|${GIGADB_PASSWORD}|g" ${APPLICATION}/protected/config/db.json
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/set_env.sh.erb /vagrant/protected/scripts/set_env.sh
-sed -i "/<% db = node\[:gigadb\]\[:db\] -%>/d" /vagrant/protected/scripts/set_env.sh \
-    && sed -i "s|<%= db\[:database\] %>|${GIGADB_DATABASE}|g" /vagrant/protected/scripts/set_env.sh \
-    && sed -i "s|<%= db\[:host\] %>|${GIGADB_HOST}|g" /vagrant/protected/scripts/set_env.sh \
-    && sed -i "s|<%= db\[:user\] %>|${GIGADB_USER}|g" /vagrant/protected/scripts/set_env.sh \
-    && sed -i "s|<%= db\[:password\] %>|${GIGADB_PASSWORD}|g" /vagrant/protected/scripts/set_env.sh
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/set_env.sh.erb ${APPLICATION}/protected/scripts/set_env.sh \
+	&& sed -i "/<% db = node\[:gigadb\]\[:db\] -%>/d" ${APPLICATION}/protected/scripts/set_env.sh \
+    && sed -i "s|<%= db\[:database\] %>|${GIGADB_DATABASE}|g" ${APPLICATION}/protected/scripts/set_env.sh \
+    && sed -i "s|<%= db\[:host\] %>|${GIGADB_HOST}|g" ${APPLICATION}/protected/scripts/set_env.sh \
+    && sed -i "s|<%= db\[:user\] %>|${GIGADB_USER}|g" ${APPLICATION}/protected/scripts/set_env.sh \
+    && sed -i "s|<%= db\[:password\] %>|${GIGADB_PASSWORD}|g" ${APPLICATION}/protected/scripts/set_env.sh
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/es.json.erb /vagrant/protected/config/es.json
-sed -i "s|<%= node\[:gigadb\]\[:es_port\] %>|${GIGADB_ES_PORT}|g" /vagrant/protected/config/es.json
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/es.json.erb ${APPLICATION}/protected/config/es.json \
+	&& sed -i "s|<%= node\[:gigadb\]\[:es_port\] %>|${GIGADB_ES_PORT}|g" ${APPLICATION}/protected/config/es.json
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/update_links.sh.erb /vagrant/protected/scripts/update_links.sh
-sed -i "s|<%= node\[:gigadb\]\[:db\]\[:password\] %>|${GIGADB_PASSWORD}|g" /vagrant/protected/scripts/update_links.sh
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/update_links.sh.erb ${APPLICATION}/protected/scripts/update_links.sh \
+	&& sed -i "s|<%= node\[:gigadb\]\[:db\]\[:password\] %>|${GIGADB_PASSWORD}|g" ${APPLICATION}/protected/scripts/update_links.sh
 
-cp /vagrant/chef/site-cookbooks/gigadb/templates/default/yii-help.html.erb /vagrant/files/html/help.html
-sed -i "/<% path = node\[:yii\]\[:ip_address\] -%>/d" /vagrant/files/html/help.html \
-    && sed -i "s|<%= path %>|${HOME_URL}|g" /vagrant/files/html/help.html
+cp ${APPLICATION}/chef/site-cookbooks/gigadb/templates/default/yii-help.html.erb ${APPLICATION}/files/html/help.html \
+	&& sed -i "/<% path = node\[:yii\]\[:ip_address\] -%>/d" ${APPLICATION}/files/html/help.html \
+    && sed -i "s|<%= path %>|${HOME_URL}|g" ${APPLICATION}/files/html/help.html
 
 # Download Yii version 1
-mkdir -p ${YII_PATH}
-git clone git://github.com/yiisoft/yii.git -b master ${YII_PATH}
+if ! [ -f  yii-1.1.16.tar.gz ];then
+	curl -o yii-1.1.16.tar.gz -L https://github.com/yiisoft/yii/releases/download/1.1.16/yii-1.1.16.bca042.tar.gz
+	tar xvzf yii-1.1.16.tar.gz --directory /opt/ --transform 's/yii-1.1.16.bca042/yii-1.1.16/'
+fi
 
 # Generate site.css
-/vagrant/protected/yiic lesscompiler
+${APPLICATION}/protected/yiic lesscompiler
 
 # Download example dataset files
-mkdir -p /vagrant/yii2-laradock/vsftpd/files
-curl -o /vagrant/yii2-laradock/vsftpd/files/ftpexamples4.tar.gz https://s3-ap-southeast-1.amazonaws.com/gigadb-ftp-sample-data/ftpexamples4.tar.gz
-tar -xzvf /vagrant/yii2-laradock/vsftpd/files/ftpexamples4.tar.gz -C /vagrant/yii2-laradock/vsftpd/files
+mkdir -p ${APPLICATION}/vsftpd/files
+if ! [ -f ${APPLICATION}/vsftpd/files/ftpexamples4.tar.gz ]; then
+  curl -o ${APPLICATION}/vsftpd/files/ftpexamples4.tar.gz https://s3-ap-southeast-1.amazonaws.com/gigadb-ftp-sample-data/ftpexamples4.tar.gz
+fi
+files_count=$(ls -1 ${APPLICATION}/yii2-laradock/vsftpd/files | wc -l)
+if ! [ $files_count -eq 11 ]; then
+  tar -xzvf ${APPLICATION}/vsftpd/files/ftpexamples4.tar.gz -C ${APPLICATION}/vsftpd/files
+fi
